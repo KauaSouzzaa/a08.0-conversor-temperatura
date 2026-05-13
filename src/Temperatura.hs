5e49b6c8fb30eac4ccde2d4dba8803fd04b9b22b
Aluno <JoaoP259>
2026-05-13 11:33:04 -0300

    Foi Resolvido o exercicio

--- src/Temperatura.hs
+++ src/Temperatura.hs
@@ -6,9 +6,32 @@ import Text.Printf (printf)
 import Text.Read (readMaybe)
 
 converteTemperatura :: Double -> Maybe (Double, Double)
-converteTemperatura celsius =
-  undefined
+converteTemperatura celsius
+ | celsius < zeroAbsoluto = Nothing
+ | otherwise = Just (fah,kel)
+ where
+    fah = (celsius*9 / 5) + 32
+    kel = celsius - zeroAbsoluto
+    zeroAbsoluto = -273.15
 
 main :: IO ()
-main =
-  undefined
+main = do
+        hSetBuffering stdout NoBuffering
+        putStr "Digite a temperatura em Celsius: "
+        entrada <- getLine
+        let maybeTemperatura = readMaybe entrada :: Maybe Double
+        if isNothing maybeTemperatura
+          then
+            putStrLn "Entrada inválida!"
+          else do
+            let temp = converteTemperatura (fromJust maybeTemperatura)
+            if isNothing temp
+              then 
+                putStrLn "Entrada inválida!"
+              else do
+                let fah = fst (fromJust temp)
+                let kel = snd (fromJust temp)
+
+                putStr (printf "Temperatura em Fahrenheit: %.2f\n" fah :: String)
+                putStr (printf "Temperatura em Kelvin: %.2f\n" kel :: String)
+            
